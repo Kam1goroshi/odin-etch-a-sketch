cc2cba17fef9dbb9afe5d514d074c7a88fa18ead
Kam1goroshi <g.pappas.1@nup.ac.cy>
2022-05-31 16:20:33 +0300

    Generate canvas from javascript, clear html
    Removed the hard coded canvas from html and now actually generate it
    with js like initially intended in order to have full painting control
    over it

--- canvas.js
+++ canvas.js
@@ -1,1 +1,47 @@
-console.log("hi");
+/**
+ * Generates a pixel
+ * @param {*} size 
+ */
+const generatePixel = (size) => {
+    pixel = new Object();
+    pixel.startingColor = "#ffffff";
+    const rect = document.createElement('div');
+    rect.setAttribute('class','pixel');
+    rect.style.backgroundColor = pixel.startingColor;
+    rect.style.width = `${size}px`;
+    rect.style.height = `${size}px`;
+    pixel.element = rect;
+    return pixel;
+}
+
+// Was doing: adding row element to canvas object
+// to do: add pixels as children to that row, clear css
+const generateCanvas = (pixelCount, pixelSize) => {
+    const canvas = new Object();
+    canvas.pixelArray = new Array(pixelCount);
+    canvas.pixelRows = new Array(pixelCount);
+    for (let i = 0; i < pixelCount; i++) {
+        canvas.pixelArray[i] = new Array(pixelCount);
+        canvas.pixelRows[i] = document.createElement('div');
+        canvas.pixelRows[i].setAttribute('class','row');
+        for (let j = 0; j < pixelCount; j++) {
+            canvas.pixelArray[i][j] = generatePixel(pixelSize);
+            canvas.pixelRows[i].appendChild(canvas.pixelArray[i][j].element);
+        }
+    }
+    return canvas;
+}
+
+// const deleteCurrentCanvas(){
+
+// }
+
+const displayCanvas = (canvas) => {
+    canvas.pixelRows.forEach(element => {
+        document.querySelector(".canvas").appendChild(element);
+    });
+}
+
+const canvas = generateCanvas(16, 50);
+displayCanvas(canvas);
+console.log(canvas);
